b07942a30f4cbb5d76e40661bd4e6c8e513236be
jarod <jarodcthornton@gmail.com>
2022-05-16 19:51:45 -0400

    Initial commit

---
+++ src/app/roulette-wheel/roulette-wheel.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RouletteWheelComponent } from './roulette-wheel.component';
+
+describe('RouletteWheelComponent', () => {
+  let component: RouletteWheelComponent;
+  let fixture: ComponentFixture<RouletteWheelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RouletteWheelComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RouletteWheelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/roulette-wheel/roulette-wheel.component.ts
@@ -0,0 +1,104 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {genreData} from "../../assets/genre-data";
+
+@Component({
+  selector: 'app-roulette-wheel',
+  templateUrl: './roulette-wheel.component.html',
+  styleUrls: ['./roulette-wheel.component.css'],
+})
+export class RouletteWheelComponent implements OnInit {
+  public genres: string[] = []
+  public moviePick: string | undefined
+  public arrOfVowels: string[] = ['A', 'E', 'I', 'O', 'U']
+  public sentenceStructure: string
+  public genreArray = genreData
+  public url: string
+  public movieFilter: number
+  public spinButton: string = "Spin"
+
+
+  spinTheWheel() {
+    this.moviePick = this.genres[Math.floor(Math.random() * this.genres.length)]
+    if (this.arrOfVowels.includes(this.moviePick[0])) {
+      this.sentenceStructure = "an " + this.moviePick
+    }
+    else {
+      this.sentenceStructure = "a " + this.moviePick
+    }
+    let selectedGenre = this.moviePick
+    this.url = genreData.find(function(item){
+      return item.genre == selectedGenre
+    }).url
+    console.log(this.moviePick)
+    switch (this.moviePick) {
+      case "Action":
+      case "Adventure":
+      case "Crime":
+      case "Family":
+      case "Fantasy":
+      case "Film-Noir":
+      case "Horror":
+      case "History":
+      case "Music":
+      case "Mystery":
+      case "Romance":
+      case "Sci-Fi":
+      case "Superhero":
+      case "War":
+        this.movieFilter = 1
+        break;
+      case "Animation":
+      case "Biography":
+      case "Comedy":
+      case "Documentary":
+      case "Drama":
+      case "Game-Show":
+      case "Musical":
+      case "Short":
+      case "Talk-Show":
+      case "Thriller":
+      case "Western":
+        this.movieFilter = 2
+        break;
+      case "Reality-TV":
+        this.movieFilter = 3
+        break;
+      case "Sport":
+        this.movieFilter = 4
+        break;
+      default:
+        console.log("switch case hit the default condition - somethings not right")
+    }
+    this.spinButton = "Spin Again"
+  };
+
+  findGenre() {
+    window.open(this.url, "_blank");
+  }
+
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  ngOnInit(): void {
+    let headers = new HttpHeaders({
+      'X-RapidAPI-Host': 'data-imdb1.p.rapidapi.com',
+      'X-RapidAPI-Key': 'eaa56f021cmsh1d4c38315bf58ccp17d3e6jsna3f17453c0bd'
+    });
+    this.http
+      .get<GenreDataWrapper>('https://data-imdb1.p.rapidapi.com/titles/utils/genres', {
+        headers: headers
+      })
+      .subscribe(data => {
+        this.genres = data.results.filter(genre => genre);
+        this.genres.splice(1, 1)
+        this.genres.splice(17, 1)
+      });
+  }
+}
+
+interface GenreDataWrapper {
+  results: string[]
+}
